8368a9d4b00de85edc3fe3cbfe012fc7a47fa034
Dimo99 <dimodimov1999@gmail.com>
2024-09-18 14:24:23 -0400

    feat: Use azure

--- ts/init.ts
+++ ts/init.ts
@@ -1,57 +1,64 @@
-import { ArgumentParser } from 'argparse'
-import * as shelljs from 'shelljs'
-import * as fs from 'fs'
-import * as path from 'path'
-import { FORMAT, validateZkeyDir } from './utils'
-
+import { ArgumentParser } from "argparse";
+import * as shelljs from "shelljs";
+import * as fs from "fs";
+import * as path from "path";
+import {
+    computeB3sum,
+    FORMAT,
+    getZkeyFiles,
+    AZURE_SAS_TOKEN,
+    validateZkeyDir,
+} from "./utils";
+import MerkleTree from "merkletreejs";
 
 const configureSubparsers = (subparsers: ArgumentParser) => {
-    const parser = subparsers.add_parser(
-        'init',
-        { add_help: true },
-    )
+    const parser = subparsers.add_parser("init", { add_help: true });
 
-    parser.add_argument(
-        '-d',
-        '--dir',
-        {
-            required: true,
-            action: 'store',
-            type: 'str',
-            help: 'The directory that contains the .zkey files. Each .zkey ' +
-                'file must follow this naming scheme: ' + FORMAT
-        }
-    )
-}
+    parser.add_argument("-d", "--dir", {
+        required: true,
+        action: "store",
+        type: "str",
+        help:
+            "The directory that contains the .zkey files. Each .zkey " +
+            "file must follow this naming scheme: " +
+            FORMAT,
+    });
+};
 
-const init = async (
-    dirname: string,
-) => {
+const init = async (dirname: string) => {
     if (!fs.existsSync(dirname)) {
-        console.error(`Error: ${dirname} does not exist`)
-        return 1
+        console.error(`Error: ${dirname} does not exist`);
+        return 1;
     }
 
-    const isZkeyDirValid = validateZkeyDir(dirname) === 0
+    const isZkeyDirValid = validateZkeyDir(dirname) === 0;
     if (!isZkeyDirValid) {
-        return 1
+        return 1;
     }
 
-    const cmd = `ipfs add --pin -r ${dirname}`
-    const out = shelljs.exec(cmd)
+    const zkeyFiles = getZkeyFiles(dirname);
+
+    let hashes = zkeyFiles.map((z) => computeB3sum(z.filename));
+
+    const tree = new MerkleTree(hashes);
+
+    const root = tree.getRoot().toString("hex");
+
+    const cmd = `azcopy cp "${dirname}/*" "https://dendrethstorage.blob.core.windows.net/trusted-setup/${root}?${AZURE_SAS_TOKEN}" --recursive`;
+
+    const out = shelljs.exec(cmd);
     if (out.code !== 0 || out.stderr) {
-        console.error(`Error: could not add ${dirname} to IPFS.`)
-        console.error(out.stderr)
-        return 1
+        console.error(`Error: could not add ${dirname} to S3.`);
+        console.error(out.stderr);
+        return 1;
     }
-    const multihash = out.stdout.trim()
-    console.log('Ceremony initialised. Please give this multihash to the first participant and keep your IPFS node running and connected to the IPFS network.')
-    console.log(multihash)
 
-    return 0
-}
+    console.log(
+        "Ceremony initialised. Please give this root to the first participant."
+    );
+    console.log(root);
 
-export {
-    init,
-    configureSubparsers,
-}
+    return 0;
+};
+
+export { init, configureSubparsers };
--- ts/upload.ts
+++ ts/upload.ts
@@ -1,63 +1,64 @@
-import { ArgumentParser } from 'argparse'
-import * as crypto from 'crypto'
-import * as shelljs from 'shelljs'
-import * as fs from 'fs'
-import * as path from 'path'
+import { ArgumentParser } from "argparse";
+import * as shelljs from "shelljs";
+import * as fs from "fs";
 
 import {
+    AZURE_SAS_TOKEN,
     FORMAT,
+    computeB3sum,
     countDirents,
-} from './utils'
+    getZkeyFiles,
+} from "./utils";
+import MerkleTree from "merkletreejs";
 
 const configureSubparsers = (subparsers: ArgumentParser) => {
-    const parser = subparsers.add_parser(
-        'upload',
-        { add_help: true },
-    )
+    const parser = subparsers.add_parser("upload", { add_help: true });
 
-    parser.add_argument(
-        '-d',
-        '--dir',
-        {
-            required: true,
-            action: 'store',
-            type: 'str',
-            help: 'The directory that contains the .zkey files. Each .zkey ' +
-                'file must follow this naming scheme: ' + FORMAT
-        }
-    )
+    parser.add_argument("-d", "--dir", {
+        required: true,
+        action: "store",
+        type: "str",
+        help:
+            "The directory that contains the .zkey files. Each .zkey " +
+            "file must follow this naming scheme: " +
+            FORMAT,
+    });
+};
 
-}
-
-const upload = async (
-    dirname: string,
-) => {
+const upload = async (dirname: string) => {
     if (!fs.existsSync(dirname)) {
-        console.log(`Error: ${dirname} does not exist`)
+        console.log(`Error: ${dirname} does not exist`);
     }
 
     // The directory must not be empty
-    const numFiles = countDirents(dirname)
+    const numFiles = countDirents(dirname);
     if (numFiles === 0) {
-        console.error(`Error: ${dirname} should not be empty`)
-        return 1
+        console.error(`Error: ${dirname} should not be empty`);
+        return 1;
     }
+
+    const zkeyFiles = getZkeyFiles(dirname);
+
+    let hashes = zkeyFiles.map((z) => computeB3sum(z.filename));
+
+    const tree = new MerkleTree(hashes);
+
+    const root = tree.getRoot().toString("hex");
 
     // Upload files
-    const cmd = `ipfs add --pin -Q -r ${dirname}`
-    const out = shelljs.exec(cmd, { silent: true })
+    const cmd = `azcopy cp "${dirname}/*" "https://dendrethstorage.blob.core.windows.net/trusted-setup/${root}?${AZURE_SAS_TOKEN}" --recursive`;
+    const out = shelljs.exec(cmd, { silent: true });
     if (out.code !== 0 || out.stderr) {
-        console.error(`Error: could not add ${dirname} to IPFS.`)
-        console.error(out.stderr)
-        return 1
+        console.error(`Error: could not add ${dirname} to Azure.`);
+        console.error(out.stderr);
+        return 1;
     }
-    const multihash = out.stdout.trim()
-    console.log('Contribution uploaded. Please send this multihash to the coordinator and keep your IPFS node running and connected to the IPFS network.')
-    console.log(multihash)
-    return 0
-}
 
-export {
-    upload,
-    configureSubparsers,
-}
+    console.log(
+        "Contribution uploaded. Please send this Hash root to the coordinator."
+    );
+    console.log(root);
+    return 0;
+};
+
+export { upload, configureSubparsers };
